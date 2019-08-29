b264d472ff0fc1f25819e6b18306db4a5e27b4c0
hunter <hunter.klein@huskers.unl.edu>
2019-08-29 12:50:16 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: hunter klein
+ * Date: 2019/8/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
